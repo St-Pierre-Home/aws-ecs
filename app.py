b19c476ce88d60764c7acbab63e6ee40e705832b
Marc St. Pierre <mpstpierre@gmail.com>
2022-11-05 17:07:04 -0400

    push image

--- app.py
+++ app.py
@@ -15,5 +15,6 @@ def returnBackwardsString(random_string):
     LOGGER.info('Received a message: %s', random_string)
     return "".join(reversed(random_string))
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
